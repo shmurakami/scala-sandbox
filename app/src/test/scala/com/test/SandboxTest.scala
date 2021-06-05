78b5c7f4beb2fa682498d3dffba15112113ff96d
shaka <shmurakami1104@gmail.com>
2021-06-05 15:27:19 +0900

    variance

--- app/src/test/scala/com/test/SandboxTest.scala
+++ app/src/test/scala/com/test/SandboxTest.scala
@@ -19,6 +19,38 @@ class SandboxTest extends ScalaTestWithActorTestKit(ActorTestKit()) with AnyWord
 
       true shouldBe true
     }
+
+    "sample" in {
+      sealed trait Co
+      case class Co1() extends Co
+      case class Co2() extends Co
+
+      class C[T]
+      val c1: C[Co] = new C[Co]
+
+      class C2[+T]
+      val c2: C2[Co] = new C2[Co1]
+
+      class C3[-T]
+      val c3: C3[Co2] = new C3[Co]
+
+      trait T4[T]
+      class Dispatcher extends T4[Co] {
+        def d(t: Co): Unit = ()
+      }
+      val dispatcher = new Dispatcher
+      dispatcher.d(Co1())
+
+      trait T5[T]
+      class Dispatcher5 extends T5[Co] {
+        def d(t: Co): Unit = ()
+      }
+      val dispatcher5 = new Dispatcher5
+      dispatcher5.d(Co2())
+
+      true shouldBe true
+
+    }
   }
 
 }
